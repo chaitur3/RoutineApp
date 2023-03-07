4442e681553171c57767ccab47f4862cc8436988
chaitur3 <thathireddyc@macstudent.com.au>
2023-03-07 11:26:49 +1100

    Started java code for a save button under each day.

---
+++ routineapp.js
@@ -0,0 +1,40 @@
+const mondayInput = document.querySelector('input[name="monday"]')
+const tuesdayInput = document.querySelector('input[name="tuesday"]')
+const wednesdayInput = document.querySelector('input[name="wednesday"]')
+const thursdayInput = document.querySelector('input[name="thursday"]')
+const fridayInput = document.querySelector('input[name="friday"]')
+const saturdayInput = document.querySelector('input[name="saturday"]')
+const sundayInput = document.querySelector('input[name="sunday"]')
+
+mondayInput.addEventListener('input', () => {
+    localStorage.setItem('mondayTask', mondayInput.value);
+});
+
+tuesdayInput.addEventListener('input', () => {
+    localStorage.setItem('tuesdayTask', tuesdayInput.value);
+});
+
+wednesdayInput.addEventListener('input', () => {
+    localStorage.setItem('wednesdayTask', wednesdayInput.value);
+});
+
+thursdayInput.addEventListener('input', () => {
+    localStorage.setItem('thursdayTask', thursdayInput.value);
+});
+
+fridayInput.addEventListener('input', () => {
+    localStorage.setItem('fridayTask', fridayInput.value);
+});
+
+saturdayInput.addEventListener('input', () => {
+    localStorage.setItem('saturdayTask', saturdayInput.value);
+});
+
+sundayInput.addEventListener('input', () => {
+    localStorage.setItem('sundayTask', sundayInput.value);
+});
+
+
+
+
+
